9da051cfe39f371088bc55406af71e65f68842d5
Gabor Javorszky <gabor@javorszky.co.uk>
2020-04-25 13:35:17 +0100

    Add piece adjacents and tests

--- pkg/game/piece.go
+++ pkg/game/piece.go
@@ -1,10 +1,101 @@
 package game
 
-import "go/types"
-
 type Piece struct {
-	Player Player
-	Origin types.Tuple
-	Width  int8
-	Height int8
+	Player         Player
+	Origin         Coordinate
+	AdjacentFields []Coordinate
+	Corners        []Coordinate
+	Width          uint8
+	Height         uint8
+}
+
+func NewPiece(player Player, originX uint8, originY uint8, width uint8, height uint8) Piece {
+	p := Piece{
+		Player: player,
+		Origin: Coordinate{
+			X: originX,
+			Y: originY,
+		},
+		Width:  width,
+		Height: height,
+	}
+
+	p.AdjacentFields = p.getAdjacentFields()
+	p.Corners = p.getCorners()
+
+	return p
+}
+
+type Coordinate struct {
+	X uint8
+	Y uint8
+}
+
+func (p Piece) getCorners() []Coordinate {
+	var c []Coordinate
+
+	// 0 -> X
+	// |
+	// v
+	// Y
+
+	// Origin coordinate is always the top left corner
+	c = append(c, p.Origin)
+
+	// Top right
+	c = append(c, Coordinate{X: p.Origin.X + p.Width - 1, Y: p.Origin.Y})
+
+	// Bottom right
+	c = append(c, Coordinate{X: p.Origin.X + p.Width - 1, Y: p.Origin.Y + p.Height - 1})
+
+	// Bottom left
+	c = append(c, Coordinate{X: p.Origin.X, Y: p.Origin.Y + p.Height - 1})
+
+	return c
+}
+
+func (p Piece) IsCoordinateWithin(c Coordinate) bool {
+	// check for X
+	if c.X >= p.Origin.X && c.X <= p.Origin.X+p.Width-1 && c.Y >= p.Origin.Y && c.Y <= p.Origin.Y+p.Height-1 {
+		return true
+	}
+
+	return false
+}
+
+func (p Piece) IsAdjacent(c Coordinate) bool {
+	for _, val := range p.AdjacentFields {
+		if c == val {
+			return true
+		}
+	}
+	return false
+}
+
+func (p Piece) getAdjacentFields() []Coordinate {
+	var a []Coordinate
+
+	// horizontals
+	for i := uint8(0); i < p.Width; i++ {
+		a = append(a, Coordinate{
+			X: p.Origin.X + i,
+			Y: p.Origin.Y - 1,
+		}, Coordinate{
+			X: p.Origin.X + i,
+			Y: p.Origin.Y + p.Height,
+		})
+	}
+
+	// verticals
+	for i := uint8(0); i < p.Height; i++ {
+		a = append(a, Coordinate{
+			X: p.Origin.X - 1,
+			Y: p.Origin.Y + i,
+		}, Coordinate{
+			X: p.Origin.X + p.Width,
+			Y: p.Origin.Y + i,
+		})
+	}
+
+	return a
 }
--- pkg/game/piece_test.go
+++ pkg/game/piece_test.go
@@ -1,1 +1,218 @@
 package game_test
+
+import (
+	"reflect"
+	"testing"
+
+	"javorszky/dice-territory-game/v2/pkg/game"
+)
+
+func TestNewPiece(t *testing.T) {
+	type args struct {
+		player  game.Player
+		originX uint8
+		originY uint8
+		width   uint8
+		height  uint8
+	}
+	tests := []struct {
+		name string
+		args args
+		want game.Piece
+	}{
+		{
+			name: "successfully create piece",
+			args: args{
+				player: game.Player{
+					ID:    "playerid-1",
+					Name:  "John",
+					Score: 0,
+				},
+				originX: 4,
+				originY: 6,
+				width:   4,
+				height:  3,
+			},
+			want: game.Piece{
+				Player: game.Player{
+					ID:    "playerid-1",
+					Name:  "John",
+					Score: 0,
+				},
+				Origin: game.Coordinate{X: 4, Y: 6},
+				AdjacentFields: []game.Coordinate{
+					{X: 4, Y: 5},
+					{X: 4, Y: 9},
+					{X: 5, Y: 5},
+					{X: 5, Y: 9},
+					{X: 6, Y: 5},
+					{X: 6, Y: 9},
+					{X: 7, Y: 5},
+					{X: 7, Y: 9},
+					{X: 3, Y: 6},
+					{X: 8, Y: 6},
+					{X: 3, Y: 7},
+					{X: 8, Y: 7},
+					{X: 3, Y: 8},
+					{X: 8, Y: 8},
+				},
+				Corners: []game.Coordinate{
+					{X: 4, Y: 6},
+					{X: 7, Y: 6},
+					{X: 7, Y: 8},
+					{X: 4, Y: 8},
+				},
+				Width:  4,
+				Height: 3,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := game.NewPiece(tt.args.player, tt.args.originX, tt.args.originY, tt.args.width, tt.args.height); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewPiece() = \n%v, want \n%v", got, tt.want)
+			}
+		})
+	}
+}
+
+//
+//func TestPiece_IsAdjacent(t *testing.T) {
+//	type fields struct {
+//		Player         game.Player
+//		Origin         game.Coordinate
+//		AdjacentFields []Coordinate
+//		Corners        []Coordinate
+//		Width          uint8
+//		Height         uint8
+//	}
+//	type args struct {
+//		c game.Coordinate
+//	}
+//	tests := []struct {
+//		name   string
+//		fields fields
+//		args   args
+//		want   bool
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			p := game.Piece{
+//				Player:         tt.fields.Player,
+//				Origin:         tt.fields.Origin,
+//				AdjacentFields: tt.fields.AdjacentFields,
+//				Corners:        tt.fields.Corners,
+//				Width:          tt.fields.Width,
+//				Height:         tt.fields.Height,
+//			}
+//			if got := p.IsAdjacent(tt.args.c); got != tt.want {
+//				t.Errorf("IsAdjacent() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
+//
+//func TestPiece_IsCoordinateWithin(t *testing.T) {
+//	type fields struct {
+//		Player         game.Player
+//		Origin         game.Coordinate
+//		AdjacentFields []Coordinate
+//		Corners        []Coordinate
+//		Width          uint8
+//		Height         uint8
+//	}
+//	type args struct {
+//		c game.Coordinate
+//	}
+//	tests := []struct {
+//		name   string
+//		fields fields
+//		args   args
+//		want   bool
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			p := game.Piece{
+//				Player:         tt.fields.Player,
+//				Origin:         tt.fields.Origin,
+//				AdjacentFields: tt.fields.AdjacentFields,
+//				Corners:        tt.fields.Corners,
+//				Width:          tt.fields.Width,
+//				Height:         tt.fields.Height,
+//			}
+//			if got := p.IsCoordinateWithin(tt.args.c); got != tt.want {
+//				t.Errorf("IsCoordinateWithin() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
+//
+//func TestPiece_getAdjacentFields(t *testing.T) {
+//	type fields struct {
+//		Player         game.Player
+//		Origin         game.Coordinate
+//		AdjacentFields []Coordinate
+//		Corners        []Coordinate
+//		Width          uint8
+//		Height         uint8
+//	}
+//	tests := []struct {
+//		name   string
+//		fields fields
+//		want   []Coordinate
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			p := game.Piece{
+//				Player:         tt.fields.Player,
+//				Origin:         tt.fields.Origin,
+//				AdjacentFields: tt.fields.AdjacentFields,
+//				Corners:        tt.fields.Corners,
+//				Width:          tt.fields.Width,
+//				Height:         tt.fields.Height,
+//			}
+//			if got := p.getAdjacentFields(); !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("getAdjacentFields() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
+//
+//func TestPiece_getCorners(t *testing.T) {
+//	type fields struct {
+//		Player         game.Player
+//		Origin         game.Coordinate
+//		AdjacentFields []Coordinate
+//		Corners        []Coordinate
+//		Width          uint8
+//		Height         uint8
+//	}
+//	tests := []struct {
+//		name   string
+//		fields fields
+//		want   []Coordinate
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			p := game.Piece{
+//				Player:         tt.fields.Player,
+//				Origin:         tt.fields.Origin,
+//				AdjacentFields: tt.fields.AdjacentFields,
+//				Corners:        tt.fields.Corners,
+//				Width:          tt.fields.Width,
+//				Height:         tt.fields.Height,
+//			}
+//			if got := p.getCorners(); !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("getCorners() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
